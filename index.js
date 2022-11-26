64128179c4c4abfbe92147326a4cc7ebff070067
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-26 19:31:30 +0600

    added category api

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ async function run() {
         })
 
 
+
+        app.get('/category/:name', async (req, res) => {
+            const name = req.params.name;
+            console.log(name);
+            const query = {}
+            const allBooks = await booksCollection.find(query).toArray();
+
+            const category_books = allBooks.filter(c => c.category === name);
+            res.send(category_books)
+
+
+        })
+
         // get all books
         app.get('/allBooks', async (req, res) => {
             const query = {}
@@ -40,13 +53,6 @@ async function run() {
             const allBooks = await cursor.toArray();
             res.send(allBooks)
         })
-
-        // app.post('/services', async (req, res) => {
-        //     const service = req.body;
-        //     const result = await serviceCollection.insertOne(service);
-        //     res.send(result)
-
-        // })
 
 
 
@@ -57,13 +63,6 @@ async function run() {
             res.send(books)
         })
 
-
-        // app.get('/services/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) }
-        //     const service = await serviceCollection.findOne(query);
-        //     res.send(service)
-        // })
 
 
 
