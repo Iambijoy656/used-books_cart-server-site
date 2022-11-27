5c43291f216b34233705a000fd3d366ad7aaf6b8
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-27 23:29:06 +0600

    added user collection

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ async function run() {
         const categoriesCollection = client.db('booksCart').collection('categories')
         const booksCollection = client.db('booksCart').collection('allBooks')
 
+        const usersCollection = client.db('booksCart').collection('users')
+
         // get all categories
         app.get('/categories', async (req, res) => {
             const query = {};
@@ -64,6 +66,46 @@ async function run() {
         })
 
 
+
+        // Users
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users)
+        })
+
+
+        // app.get('/users/admin/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email }
+        //     const user = await usersCollection.findOne(query)
+        //     res.send({ isAdmin: user?.role === 'admin' })
+        // })
+
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+
+        })
+
+
+        // app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: ObjectId(id) }
+        //     const options = { upsert: true }
+        //     const updatedDoc = {
+        //         $set: {
+        //             role: 'admin',
+        //         }
+        //     }
+        //     const result = await usersCollection.updateOne(filter, updatedDoc, options)
+
+        //     res.send(result)
+        // })
 
 
 
